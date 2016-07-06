1a29f743d4924befa859608389e993704ecd3bec
apkrieg <andrew@kriegtech.com>
2016-07-06 02:09:57 -0400

    Refactor

---
+++ main/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	fvm "github.com/apkrieg/foovm"
+)
+
+var (
+	Program = []byte{
+		fvm.Push, 16,
+		fvm.Push, 0,
+		fvm.Push, 12,
+		fvm.Push, 0,
+		fvm.Push, 255,
+		fvm.Call,
+		fvm.Push, 1,
+		fvm.Push, 255,
+		fvm.Call,
+		'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!',
+		
+	}
+)
+
+func main() {
+	vm := fvm.New()
+	for i, v := range Program {
+		vm.Heap[i] = v
+	}
+	vm.Exec()
+}
